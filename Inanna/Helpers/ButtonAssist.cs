e4f1768f7b0be84036d7bd29d7ed971730676f43
gubard <maksimovsergey08@gmail.com>
2025-12-02 09:20:37 +0200

    Add support for dynamic password visibility toggling using pseudo-classes

--- Inanna/Helpers/ButtonAssist.cs
+++ Inanna/Helpers/ButtonAssist.cs
@@ -2,7 +2,6 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.VisualTree;
-using IconPacks.Avalonia.MaterialDesign;
 
 namespace Inanna.Helpers;
 
@@ -23,19 +22,46 @@ public static class ButtonAssist
 
     static ButtonAssist()
     {
-        IsPasswordShowProperty.Changed.AddClassHandler<Button, bool>((s, e) =>
+        IsPasswordShowProperty.Changed.AddClassHandler<Button, bool>((button, e) =>
         {
             if (e.NewValue.GetValueOrDefault<bool>())
             {
-                s.Click += OnClick;
+                button.Click += OnClick;
+                button.SetPseudoClass(":show-password", true);
+                button.SetPseudoClass(":hide-password", false);
+                var passwordTextBox = GetPasswordTextBox(button);
+
+                if (passwordTextBox is null)
+                {
+                    return;
+                }
+
+                passwordTextBox.SetPseudoClass(":show-password", true);
+                passwordTextBox.SetPseudoClass(":hide-password", false);
             }
             else
             {
-                s.Click -= OnClick;
+                button.Click -= OnClick;
+                button.SetPseudoClass(":show-password", false);
+                button.SetPseudoClass(":hide-password", false);
+
+                var passwordTextBox = GetPasswordTextBox(button);
+
+                if (passwordTextBox is null)
+                {
+                    return;
+                }
+
+                passwordTextBox.SetPseudoClass(":show-password", false);
+                passwordTextBox.SetPseudoClass(":hide-password", false);
             }
         });
     }
 
+    private static TextBox? GetPasswordTextBox(Button button)
+    {
+        return button.GetVisualAncestors().OfType<TextBox>().FirstOrDefault();
+    }
 
     private static void OnClick(object? sender, RoutedEventArgs e)
     {
@@ -44,34 +70,30 @@ public static class ButtonAssist
             return;
         }
 
-        var textBox = button.GetVisualAncestors().OfType<TextBox>().FirstOrDefault();
+        var passwordTextBox = GetPasswordTextBox(button);
 
-        if (textBox is null)
+        if (passwordTextBox is null)
         {
             return;
         }
 
-        switch (textBox.PasswordChar)
+        switch (passwordTextBox.PasswordChar)
         {
             case '\0':
             {
-                textBox.PasswordChar = '*';
-
-                button.Content = new PackIconMaterialDesign
-                {
-                    Kind = PackIconMaterialDesignKind.Visibility,
-                };
+                button.SetPseudoClass(":show-password", true);
+                button.SetPseudoClass(":hide-password", false);
+                passwordTextBox.SetPseudoClass(":show-password", true);
+                passwordTextBox.SetPseudoClass(":hide-password", false);
 
                 break;
             }
             default:
             {
-                textBox.PasswordChar = '\0';
-
-                button.Content = new PackIconMaterialDesign
-                {
-                    Kind = PackIconMaterialDesignKind.VisibilityOff,
-                };
+                button.SetPseudoClass(":show-password", false);
+                button.SetPseudoClass(":hide-password", true);
+                passwordTextBox.SetPseudoClass(":show-password", false);
+                passwordTextBox.SetPseudoClass(":hide-password", true);
 
                 break;
             }
---
+++ Inanna/Helpers/StyledElementExtension.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace Inanna.Helpers;
+
+public static class StyledElementExtension
+{
+    public static void SetPseudoClass<TStyledElement>(this TStyledElement styledElement, string pseudoClass, bool value) where TStyledElement : StyledElement
+    {
+        if (styledElement.Classes is IPseudoClasses classes)
+        {
+            classes.Set(pseudoClass, value);
+        }
+    }
+}
