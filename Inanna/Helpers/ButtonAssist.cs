392e736928065bf5b9f6aeb95e192d895f041369
gubard <maksimovsergey08@gmail.com>
2025-12-02 08:16:27 +0200

    Add `ButtonAssist` helper for toggling password visibility
    Introduced the `ButtonAssist` class for managing password visibility toggle functionality in `TextBox` controls, including associated visual updates. Updated `TextBox.axaml` to leverage the helper.

---
+++ Inanna/Helpers/ButtonAssist.cs
@@ -0,0 +1,80 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.VisualTree;
+using IconPacks.Avalonia.MaterialDesign;
+
+namespace Inanna.Helpers;
+
+public static class ButtonAssist
+{
+    public static readonly AttachedProperty<bool> IsPasswordShowProperty =
+        AvaloniaProperty.RegisterAttached<Button, bool>("IsPasswordShow", typeof(ButtonAssist));
+
+    public static void SetIsDragHandle(Button element, bool value)
+    {
+        element.SetValue(IsPasswordShowProperty, value);
+    }
+
+    public static bool GetIsDragHandle(Button element)
+    {
+        return element.GetValue(IsPasswordShowProperty);
+    }
+
+    static ButtonAssist()
+    {
+        IsPasswordShowProperty.Changed.AddClassHandler<Button, bool>((s, e) =>
+        {
+            if (e.NewValue.GetValueOrDefault<bool>())
+            {
+                s.Click += OnClick;
+            }
+            else
+            {
+                s.Click -= OnClick;
+            }
+        });
+    }
+
+
+    private static void OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (sender is not Button button)
+        {
+            return;
+        }
+
+        var textBox = button.GetVisualAncestors().OfType<TextBox>().FirstOrDefault();
+
+        if (textBox is null)
+        {
+            return;
+        }
+
+        switch (textBox.PasswordChar)
+        {
+            case '\0':
+            {
+                textBox.PasswordChar = '*';
+
+                button.Content = new PackIconMaterialDesign
+                {
+                    Kind = PackIconMaterialDesignKind.Visibility,
+                };
+
+                break;
+            }
+            default:
+            {
+                textBox.PasswordChar = '\0';
+
+                button.Content = new PackIconMaterialDesign
+                {
+                    Kind = PackIconMaterialDesignKind.VisibilityOff,
+                };
+
+                break;
+            }
+        }
+    }
+}
