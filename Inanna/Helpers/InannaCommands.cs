66e50b3b80344e74340928fbdc2f006fc5585d75
gubard <maksimovsergey08@gmail.com>
2025-12-06 17:35:10 +0200

    Add `InannaCommands` helper class for navigation command

---
+++ Inanna/Helpers/InannaCommands.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Input;
+using Gaia.Helpers;
+using Inanna.Services;
+
+namespace Inanna.Helpers;
+
+public static class InannaCommands
+{
+    static InannaCommands()
+    {
+        var navigator = DiHelper.ServiceProvider.GetService<INavigator>();
+        NavigateToCommand = UiHelper.CreateCommand<Type>((type, ct) => navigator.NavigateToAsync(DiHelper.ServiceProvider.GetService(type), ct));
+    }
+
+    public static readonly ICommand NavigateToCommand;
+}
