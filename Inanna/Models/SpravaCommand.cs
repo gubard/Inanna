9ef64f2d8df8c796beea80d93dacbf643e0c3f6c
gubard <maksimovsergey08@gmail.com>
2025-12-08 09:31:09 +0200

    Add `SpravaCommand` model, `Int32MoreThanConverter`, and update resources/structure

---
+++ Inanna/Models/SpravaCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Input;
+
+namespace Inanna.Models;
+
+public sealed class SpravaCommand
+{
+    public SpravaCommand(ICommand command, object? parameter, object content)
+    {
+        Command = command;
+        Parameter = parameter;
+        Content = content;
+    }
+
+    public ICommand Command { get; }
+    public object? Parameter { get; }
+    public object Content { get; }
+   
+}
--- Inanna/Services/DoubleToPercentsConverter.cs
+++ Inanna/Services/DoubleToPercentsConverter.cs
@@ -48,3 +48,38 @@ public class DoubleToPercentsConverter : IValueConverter
         return double.TryParse(parameter?.ToString(), CultureInfo.InvariantCulture.NumberFormat, out percentage);
     }
 }
+
+public class Int32MoreThanConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not int number)
+        {
+            return value;
+        }
+
+        if (!TryGet(parameter, out var value2))
+        {
+            return value;
+        }
+
+        return number > value2;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+
+    private static bool TryGet([NotNullWhen(true)] object? parameter, out double percentage)
+    {
+        if (parameter is double value)
+        {
+            percentage = value;
+
+            return true;
+        }
+
+        return double.TryParse(parameter?.ToString(), CultureInfo.InvariantCulture.NumberFormat, out percentage);
+    }
+}
