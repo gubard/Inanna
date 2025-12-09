35d19db419465be0e79063ff98cf179979532966
gubard <maksimovsergey08@gmail.com>
2025-12-09 21:40:06 +0200

    Add utility extension method for AvaloniaList to support item reordering

---
+++ Inanna/Helpers/AvaloniaListExtension.cs
@@ -0,0 +1,83 @@
+﻿using Avalonia.Collections;
+
+namespace Inanna.Helpers;
+
+public static class AvaloniaListExtension
+{
+    public static void UpdateOrder<T>(this AvaloniaList<T> list, T[] items)
+        where T : notnull
+    {
+        var index = 0;
+
+        for (; index < items.Length; index++)
+        {
+            var item = items[index];
+
+            if (index >= list.Count)
+            {
+                list.Add(item);
+                
+                continue;
+            }
+     
+            if (item.Equals(list[index]))
+            {
+                continue;
+            }
+
+            var lastIndex = list.LastIndexOf(item);
+
+            if (lastIndex != -1)
+            {
+                list.RemoveAt(lastIndex);
+            }
+
+            list.Insert(index, item);
+        }
+
+        if (index != list.Count - 1)
+        {
+            list.RemoveRange(index + 1, list.Count - index - 1);
+        }
+    }
+
+    extension<T>(AvaloniaList<T> list)
+    {
+        private int LastIndexOf(T item)
+        {
+            if (list.Count == 0)
+            {
+                return -1;
+            }
+
+            return list.LastIndexOf(item, list.Count - 1, list.Count);
+        }
+
+        private int LastIndexOf(T item,
+            int index, int count)
+        {
+            if (list.Count != 0 && index < 0)
+            {
+                throw new();
+            }
+
+            if (list.Count != 0 && count < 0)
+            {
+                throw new();
+            }
+
+            if (index >= list.Count)
+            {
+                throw new();
+            }
+
+            if (count > index + 1)
+            {
+                throw new();
+            }
+
+            return Array.LastIndexOf(list.ToArray(), item, index, count);
+        }
+    }
+
+}
