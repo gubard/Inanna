1769a863006af3fc11857827a53cee0c508c8a0f
gubard <maksimovsergey08@gmail.com>
2025-12-09 19:27:05 +0200

    Add `InsertFirstElementConverter` and update `PathControl` for enhanced binding support

--- Inanna/Controls/PathControl.cs
+++ Inanna/Controls/PathControl.cs
@@ -1,71 +1,49 @@
 using System.Collections;
+using System.Collections.Specialized;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Templates;
-using Avalonia.Data;
-using Avalonia.Metadata;
 using IconPacks.Avalonia.MaterialDesign;
 
 namespace Inanna.Controls;
 
 public class PathControl : TemplatedControl
 {
-    private static readonly FuncTemplate<Control?> DefaultSeparator = new(() => new PackIconMaterialDesign
-    {
-        Kind = PackIconMaterialDesignKind.ChevronRight,
-    });
+    private static readonly FuncTemplate<Control?> DefaultSeparator = new(() =>
+        new PackIconMaterialDesign
+        {
+            Kind = PackIconMaterialDesignKind.ChevronRight,
+        });
 
-    public static readonly StyledProperty<ITemplate<Control?>> SeparatorProperty =
-        AvaloniaProperty.Register<PathControl, ITemplate<Control?>>(nameof(SeparatorPanel), DefaultSeparator);
+    public static readonly StyledProperty<ITemplate<Control?>>
+        SeparatorProperty =
+            AvaloniaProperty.Register<PathControl, ITemplate<Control?>>(
+                nameof(SeparatorPanel), DefaultSeparator);
 
     public static readonly StyledProperty<IDataTemplate?> ItemTemplateProperty =
         ItemsControl.ItemTemplateProperty.AddOwner<PathControl>();
 
-    public static readonly StyledProperty<IEnumerable?> SegmentsProperty =
-        AvaloniaProperty.Register<PathControl, IEnumerable?>(nameof(Segments), defaultBindingMode: BindingMode.TwoWay);
+    public static readonly StyledProperty<IEnumerable?> ItemsSourceProperty =
+        AvaloniaProperty.Register<PathControl, IEnumerable?>(
+            nameof(ItemsSource));
 
-    public static readonly StyledProperty<IEnumerable> ItemsProperty =
-        AvaloniaProperty.Register<PathControl, IEnumerable>(nameof(Items),
-            defaultBindingMode: BindingMode.OneWayToSource);
+    public static readonly DirectProperty<PathControl, IList?> ItemsProperty =
+        AvaloniaProperty.RegisterDirect<PathControl, IList?>(nameof(Items),
+            control => control._items);
+
+    private INotifyCollectionChanged? _notifyCollectionChanged;
+    private IList? _items;
 
     static PathControl()
     {
-        SegmentsProperty.Changed.AddClassHandler<PathControl>((control, _) =>
-        {
-            if (control.Segments is null)
+        ItemsSourceProperty.Changed.AddClassHandler<PathControl>(
+            (pc, _) =>
             {
-                return;
+                pc.UpdateDefaultItems();
+                pc.UpdateNotifyCollectionChanged();
             }
-
-            var segments = control.Segments.OfType<object>().ToArray().AsSpan();
-
-            if (segments.IsEmpty)
-            {
-                control.Items = Array.Empty<object>();
-
-                return;
-            }
-
-            if (segments.Length == 1)
-            {
-                control.Items = segments.ToArray();
-
-                return;
-            }
-
-            var body = segments[..^1];
-            var items = new object[segments.Length * 2 - 1].AsSpan();
-
-            for (var i = 0; i < body.Length; i++)
-            {
-                items[i * 2] = body[i];
-                items[i * 2 + 1] = control.SeparatorPanel.Build() ?? new object();
-            }
-
-            items[^1] = segments[^1];
-            control.Items = items.ToArray();
-        });
+        );
     }
 
     public PathControl()
@@ -79,22 +57,89 @@ public class PathControl : TemplatedControl
         set => SetValue(SeparatorProperty, value);
     }
 
-    public IEnumerable? Segments
+    public IList? Items
     {
-        get => GetValue(SegmentsProperty);
-        set => SetValue(SegmentsProperty, value);
+        get => _items;
+        private set => SetAndRaise(ItemsProperty, ref _items, value);
     }
 
-    public IEnumerable Items
-    {
-        get => GetValue(ItemsProperty);
-        private set => SetValue(ItemsProperty, value);
-    }
-
-    [InheritDataTypeFromItems(nameof(Segments))]
     public IDataTemplate? ItemTemplate
     {
         get => GetValue(ItemTemplateProperty);
         set => SetValue(ItemTemplateProperty, value);
+    }
+
+    public IEnumerable? ItemsSource
+    {
+        get => GetValue(ItemsSourceProperty);
+        set => SetValue(ItemsSourceProperty, value);
+    }
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+        UpdateDefaultItems();
+    }
+
+    private void UpdateNotifyCollectionChanged()
+    {
+        if (_notifyCollectionChanged is not null)
+        {
+            _notifyCollectionChanged.CollectionChanged -= OnCollectionChanged;
+        }
+
+        if (ItemsSource is INotifyCollectionChanged n)
+        {
+            _notifyCollectionChanged = n;
+            _notifyCollectionChanged.CollectionChanged += OnCollectionChanged;
+        }
+        else
+        {
+            _notifyCollectionChanged = null;
+        }
+    }
+
+    private void OnCollectionChanged(object? sender,
+        NotifyCollectionChangedEventArgs e)
+    {
+        UpdateDefaultItems();
+    }
+
+    private void UpdateDefaultItems()
+    {
+        if (ItemsSource is null)
+        {
+            Items = null;
+
+            return;
+        }
+
+        var segments = ItemsSource.Cast<object>().ToArray();
+
+        if (segments.Length == 0)
+        {
+            Items = Array.Empty<object>();
+
+            return;
+        }
+
+        if (segments.Length == 1)
+        {
+            Items = segments.ToArray();
+
+            return;
+        }
+
+        var body = segments[..^1];
+        var result = new object[segments.Length * 2 - 1].AsSpan();
+
+        for (var i = 0; i < body.Length; i++)
+        {
+            result[i * 2] = body[i];
+            result[i * 2 + 1] = SeparatorPanel.Build() ?? new object();
+        }
+
+        result[^1] = segments[^1];
+        Items = result.ToArray();
     }
 }
---
+++ Inanna/Services/InsertFirstElementConverter.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Inanna.Services;
+
+public class InsertFirstElementConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter,
+        CultureInfo culture)
+    {
+        if (value is null)
+        {
+            return null;
+        }
+
+        if (value is not IEnumerable enumerable)
+        {
+            return null;
+        }
+
+        return CreateNewEnumerable(enumerable, parameter);
+    }
+
+    public object? ConvertBack(object? value, Type targetType,
+        object? parameter, CultureInfo culture)
+    {
+        if (value is null)
+        {
+            return null;
+        }
+
+        if (value is not IEnumerable enumerable)
+        {
+            return null;
+        }
+
+        return BackEnumerable(enumerable);
+    }
+
+    private IEnumerable CreateNewEnumerable(IEnumerable enumerable,
+        object? parameter)
+    {
+        yield return parameter;
+
+        foreach (var item in enumerable)
+        {
+            yield return item;
+        }
+    }
+
+    private IEnumerable BackEnumerable(IEnumerable enumerable)
+    {
+        var enumerator = enumerable.GetEnumerator();
+        enumerator.MoveNext();
+
+        while (enumerator.MoveNext())
+        {
+            yield return enumerator.Current;
+        }
+
+        enumerator.Reset();
+    }
+}
