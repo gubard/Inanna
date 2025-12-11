c6acbc34bafd539a66a9b1e66870aa26902518f1
gubard <maksimovsergey08@gmail.com>
2025-12-11 14:36:38 +0200

    Add `ParametersViewModelBase` class as an abstract base for parameterized view models

---
+++ Inanna/Ui/ParametersViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using Inanna.Models;
+
+namespace Inanna.Ui;
+
+public abstract class ParametersViewModelBase : ViewModelBase
+{
+    protected readonly ValidationMode ValidationMode;
+
+    protected ParametersViewModelBase(ValidationMode validationMode, bool isShowEdit)
+    {
+        ValidationMode = validationMode;
+        IsShowEdit = isShowEdit;
+    }
+
+    public bool IsShowEdit { get; }
+}
