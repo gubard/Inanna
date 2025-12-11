0b45956ee883f67f8eb0e5637d16100544c3fb6d
gubard <maksimovsergey08@gmail.com>
2025-12-11 16:32:20 +0200

    Add color utility and brush converter logic
    - Included `ColorData` helper with predefined color definitions.
    - Added `ColorToBrushConverter` for converting colors to brushes.
    - Introduced a new `WrapPanel` style with horizontal orientation.
    - Updated resource dictionary with `ColorToBrushConverter`.
    - Removed unused view generation logic from the source generator.

---
+++ Inanna/Services/ColorToBrushConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace Inanna.Services;
+
+public sealed class ColorToBrushConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not Color color)
+        {
+            return value;
+        }
+
+        if (parameter is int a)
+        {
+            return new SolidColorBrush(new Color((byte)a, color.R, color.G, color.B));
+        }
+
+        return new SolidColorBrush(color);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not SolidColorBrush brush)
+        {
+            return value;
+        }
+
+        return brush.Color;
+    }
+}
