f867634acc4667b72e08c5ccc97c815ec9dac5ba
gubard <maksimovsergey08@gmail.com>
2025-12-15 09:43:29 +0200

    Add `HomeMark` model with singleton implementation

---
+++ Inanna/Models/HomeMark.cs
@@ -0,0 +1,11 @@
+namespace Inanna.Models;
+
+public class HomeMark
+{
+    public static readonly HomeMark Instance = new();
+    public static readonly IEnumerable<object> IEnumerableInstance = [Instance];
+    
+    private HomeMark()
+    {
+    }
+}
