8896f38e3a7e10e89998e7030687a8b3d206b4a8
gubard <maksimovsergey08@gmail.com>
2026-01-11 21:55:50 +0200

    Add `IsNullOrWhiteSpaceConverter` and `IsNotNullOrWhiteSpaceConverter`

---
+++ Inanna/Services/IsNotNullOrWhiteSpaceConverter.cs
@@ -0,0 +1,28 @@
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Gaia.Helpers;
+
+namespace Inanna.Services;
+
+public sealed class IsNotNullOrWhiteSpaceConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not string str)
+        {
+            return value;
+        }
+
+        return !str.IsNullOrWhiteSpace();
+    }
+
+    public object? ConvertBack(
+        object? value,
+        Type targetType,
+        object? parameter,
+        CultureInfo culture
+    )
+    {
+        throw new NotSupportedException();
+    }
+}
