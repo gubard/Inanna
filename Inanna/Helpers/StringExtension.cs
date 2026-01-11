aca1dead857612a2d66695c7ae4e74ba8afc6ef8
gubard <maksimovsergey08@gmail.com>
2026-01-11 19:12:29 +0200

    Add `ToDialogHeader` string extension for creating styled TextBlock

---
+++ Inanna/Helpers/StringExtension.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Inanna.Helpers;
+
+public static class StringExtension
+{
+    public static TextBlock ToDialogHeader(this string str)
+    {
+        return new() { Text = str, Classes = { "h3", "text-wrap" } };
+    }
+}
