36d975350e6d8b9c8156613a01367a6cf7d0d97b
Serhii Maksymov <maksimovsergey08@gmail.com>
2026-01-15 22:31:14 +0200

    Add `StringCutParameters` model to support string trimming with line and length limits

---
+++ Inanna/Models/StringCutParameters.cs
@@ -0,0 +1,26 @@
+using Gaia.Helpers;
+
+namespace Inanna.Models;
+
+public class StringCutParameters
+{
+    public StringCutParameters(byte maxLineCount, ushort maxLineLength)
+    {
+        MaxLineCount = maxLineCount;
+        MaxLineLength = maxLineLength;
+    }
+
+    public byte MaxLineCount { get; }
+    public ushort MaxLineLength { get; }
+
+    public string Cut(string str)
+    {
+        var lines = str.Split(Environment.NewLine);
+
+        return lines
+            .Take(MaxLineCount)
+            .Select(x => x.Length > MaxLineLength ? x.Substring(0, MaxLineLength) + "..." : x)
+            .Concat(lines.Length > MaxLineCount ? ["..."] : Enumerable.Empty<string>())
+            .JoinString(Environment.NewLine);
+    }
+}
