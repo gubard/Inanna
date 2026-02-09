4ca0588c2eed93e42731e2b95bcadca6310e7f76
Serhii Maksymov <maksimovsergey08@gmail.com>
2026-02-09 14:46:08 +0200

    Add `BusyAreaControl` with associated styles and theme integration
    - Introduce `BusyAreaControl` to manage busy states in the UI with `IsBusy` and `BusyArea` properties.
    - Add `BusyAreaControl.axaml` for control styling and behavior.
    - Integrate `BusyAreaControl` into the theme via `Theme.axaml`.

---
+++ Inanna/Controls/BusyAreaControl.cs
@@ -0,0 +1,36 @@
+using Avalonia;
+using Avalonia.Controls;
+
+namespace Inanna.Controls;
+
+public class BusyAreaControl : ContentControl
+{
+    public static readonly StyledProperty<object?> BusyAreaProperty = AvaloniaProperty.Register<
+        BusyAreaControl,
+        object?
+    >(nameof(BusyArea));
+
+    public static readonly StyledProperty<bool> IsBusyProperty = AvaloniaProperty.Register<
+        BusyAreaControl,
+        bool
+    >(nameof(IsBusy));
+
+    static BusyAreaControl()
+    {
+        IsBusyProperty.Changed.AddClassHandler<BusyAreaControl>(
+            (control, _) => control.PseudoClasses.Set(":is-busy", control.IsBusy)
+        );
+    }
+
+    public bool IsBusy
+    {
+        get => GetValue(IsBusyProperty);
+        set => SetValue(IsBusyProperty, value);
+    }
+
+    public object? BusyArea
+    {
+        get => GetValue(BusyAreaProperty);
+        set => SetValue(BusyAreaProperty, value);
+    }
+}
