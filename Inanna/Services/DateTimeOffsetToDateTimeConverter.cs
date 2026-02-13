fcdce87fd56ee284793ed426c8dbed9889d5e1b6
gubard <maksimovsergey08@gmail.com>
2026-02-13 21:52:38 +0200

    Add `DateTimeOffsetToDateTimeConverter` for value conversions and remove unused imports in `AdaptiveButtonsView.axaml.cs`.

---
+++ Inanna/Services/DateTimeOffsetToDateTimeConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Inanna.Services;
+
+public sealed class DateTimeOffsetToDateTimeConverter : IValueConverter
+{
+    public static readonly DateTimeOffsetToDateTimeConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not DateTimeOffset dateTimeOffset)
+        {
+            return value;
+        }
+
+        return dateTimeOffset.DateTime;
+    }
+
+    public object? ConvertBack(
+        object? value,
+        Type targetType,
+        object? parameter,
+        CultureInfo culture
+    )
+    {
+        if (value is not DateTime dateTime)
+        {
+            return value;
+        }
+
+        return new DateTimeOffset(dateTime);
+    }
+}
