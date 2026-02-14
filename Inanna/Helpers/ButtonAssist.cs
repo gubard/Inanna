2619e34ef92798fbf008b32663038b9f964c22a5
gubard <maksimovsergey08@gmail.com>
2026-02-14 18:57:12 +0200

    Add "select all" and "clear all" button functionality for TextBox

--- Inanna/Helpers/ButtonAssist.cs
+++ Inanna/Helpers/ButtonAssist.cs
@@ -10,6 +10,32 @@ public static class ButtonAssist
     public static readonly AttachedProperty<bool> IsPasswordHandlerProperty =
         AvaloniaProperty.RegisterAttached<Button, bool>("IsPasswordHandler", typeof(ButtonAssist));
 
+    public static readonly AttachedProperty<bool> IsClearAllHandlerProperty =
+        AvaloniaProperty.RegisterAttached<Button, bool>("IsClearAllHandler", typeof(ButtonAssist));
+
+    public static readonly AttachedProperty<bool> IsSelectAllHandlerProperty =
+        AvaloniaProperty.RegisterAttached<Button, bool>("IsSelectAllHandler", typeof(ButtonAssist));
+
+    public static void SetIsSelectAllHandler(Button element, bool value)
+    {
+        element.SetValue(IsSelectAllHandlerProperty, value);
+    }
+
+    public static bool GetIsSelectAllHandler(Button element)
+    {
+        return element.GetValue(IsSelectAllHandlerProperty);
+    }
+
+    public static void SetIsClearAllHandler(Button element, bool value)
+    {
+        element.SetValue(IsClearAllHandlerProperty, value);
+    }
+
+    public static bool GetIsClearAllHandler(Button element)
+    {
+        return element.GetValue(IsClearAllHandlerProperty);
+    }
+
     public static void SetIsPasswordHandler(Button element, bool value)
     {
         element.SetValue(IsPasswordHandlerProperty, value);
@@ -22,15 +48,43 @@ public static class ButtonAssist
 
     static ButtonAssist()
     {
+        IsSelectAllHandlerProperty.Changed.AddClassHandler<Button, bool>(
+            (button, e) =>
+            {
+                if (e.NewValue.GetValueOrDefault<bool>())
+                {
+                    button.Click += OnSelectAllClick;
+                }
+                else
+                {
+                    button.Click -= OnSelectAllClick;
+                }
+            }
+        );
+
+        IsClearAllHandlerProperty.Changed.AddClassHandler<Button, bool>(
+            (button, e) =>
+            {
+                if (e.NewValue.GetValueOrDefault<bool>())
+                {
+                    button.Click += OnClearAllClick;
+                }
+                else
+                {
+                    button.Click -= OnClearAllClick;
+                }
+            }
+        );
+
         IsPasswordHandlerProperty.Changed.AddClassHandler<Button, bool>(
             (button, e) =>
             {
                 if (e.NewValue.GetValueOrDefault<bool>())
                 {
-                    button.Click += OnClick;
+                    button.Click += OnPasswordClick;
                     button.SetPseudoClass(":show-password", true);
                     button.SetPseudoClass(":hide-password", false);
-                    var passwordTextBox = GetPasswordTextBox(button);
+                    var passwordTextBox = GetParentTextBox(button);
 
                     if (passwordTextBox is null)
                     {
@@ -42,11 +96,11 @@ public static class ButtonAssist
                 }
                 else
                 {
-                    button.Click -= OnClick;
+                    button.Click -= OnPasswordClick;
                     button.SetPseudoClass(":show-password", false);
                     button.SetPseudoClass(":hide-password", false);
 
-                    var passwordTextBox = GetPasswordTextBox(button);
+                    var passwordTextBox = GetParentTextBox(button);
 
                     if (passwordTextBox is null)
                     {
@@ -60,19 +114,54 @@ public static class ButtonAssist
         );
     }
 
-    private static TextBox? GetPasswordTextBox(Button button)
+    private static TextBox? GetParentTextBox(Button button)
     {
         return button.GetVisualAncestors().OfType<TextBox>().FirstOrDefault();
     }
 
-    private static void OnClick(object? sender, RoutedEventArgs e)
+    private static void OnSelectAllClick(object? sender, RoutedEventArgs e)
     {
         if (sender is not Button button)
         {
             return;
         }
 
-        var passwordTextBox = GetPasswordTextBox(button);
+        var textBox = GetParentTextBox(button);
+
+        if (textBox is null)
+        {
+            return;
+        }
+
+        textBox.SelectionStart = 0;
+        textBox.SelectionEnd = textBox.Text?.Length ?? 0;
+    }
+
+    private static void OnClearAllClick(object? sender, RoutedEventArgs e)
+    {
+        if (sender is not Button button)
+        {
+            return;
+        }
+
+        var textBox = GetParentTextBox(button);
+
+        if (textBox is null)
+        {
+            return;
+        }
+
+        textBox.Text = string.Empty;
+    }
+
+    private static void OnPasswordClick(object? sender, RoutedEventArgs e)
+    {
+        if (sender is not Button button)
+        {
+            return;
+        }
+
+        var passwordTextBox = GetParentTextBox(button);
 
         if (passwordTextBox is null)
         {
