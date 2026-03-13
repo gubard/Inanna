7831bdc617f06be0354423c427efd7c5ddcc1b25
gubard <maksimovsergey08@gmail.com>
2026-03-13 07:56:17 +0200

    Refactor `InannaCommands` to use instance-based design, improve dependency injection, and streamline command initialization.

---
+++ Inanna/Services/InannaCommands.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Input;
+using Gaia.Helpers;
+using Gaia.Services;
+using Inanna.Helpers;
+
+namespace Inanna.Services;
+
+public class InannaCommands
+{
+    public InannaCommands(IOpenerLink openerLink, INavigator navigator)
+    {
+        _openerLink = openerLink;
+        _navigator = navigator;
+
+        NavigateToCommand = UiHelper.CreateCommand<Type>(
+            (type, ct) => navigator.NavigateToAsync(DiHelper.ServiceProvider.GetService(type), ct)
+        );
+
+        OpenLinkCommand = UiHelper.CreateCommand<Uri>(openerLink.OpenLinkAsync);
+    }
+
+    public ICommand NavigateToCommand { get; }
+    public ICommand OpenLinkCommand { get; }
+
+    private readonly INavigator _navigator;
+    private readonly IOpenerLink _openerLink;
+}
