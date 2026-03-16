afbd7a1fa7744b741bcdc594d79b36fec3f2fadd
gubard <maksimovsergey08@gmail.com>
2026-03-16 10:53:35 +0300

    Register `IItemMutationService` in `IInannaServiceProvider`.

--- Inanna/Services/InannaServiceProvider.cs
+++ Inanna/Services/InannaServiceProvider.cs
@@ -1,8 +1,10 @@
-﻿using Jab;
+﻿using Inanna.Helpers;
+using Jab;
 
 namespace Inanna.Services;
 
 [ServiceProviderModule]
 [Transient(typeof(IInannaViewModelFactory), typeof(InannaViewModelFactory))]
+[Transient(typeof(IItemMutationService), typeof(ItemMutationService))]
 [Singleton(typeof(InannaCommands))]
 public interface IInannaServiceProvider : IServiceProvider;
