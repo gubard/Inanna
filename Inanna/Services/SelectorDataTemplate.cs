feb455e0196f3d0dd885000bdaf46f4c74e8e003
gubard <maksimovsergey08@gmail.com>
2026-06-30 16:11:46 +0300

    Update `SelectorDataTemplate` to implement `ITreeDataTemplate` and add `BindChildren` method.

--- Inanna/Services/SelectorDataTemplate.cs
+++ Inanna/Services/SelectorDataTemplate.cs
@@ -1,10 +1,12 @@
+using Avalonia;
 using Avalonia.Collections;
 using Avalonia.Controls;
 using Avalonia.Controls.Templates;
+using Avalonia.Markup.Xaml.Templates;
 
 namespace Inanna.Services;
 
-public sealed class SelectorDataTemplate : AvaloniaList<IDataTemplate>, IDataTemplate
+public sealed class SelectorDataTemplate : AvaloniaList<IDataTemplate>, ITreeDataTemplate
 {
     public SelectorDataTemplate()
     {
@@ -19,5 +21,28 @@ public sealed class SelectorDataTemplate : AvaloniaList<IDataTemplate>, IDataTem
     public bool Match(object? data)
     {
         return this.Any(x => x.Match(data));
+    }
+
+    class Disposable : IDisposable
+    {
+        public void Dispose() { }
+    }
+
+    public IDisposable BindChildren(
+        AvaloniaObject target,
+        AvaloniaProperty targetProperty,
+        object item
+    )
+    {
+        var template = this.OfType<TreeDataTemplate>().FirstOrDefault(x => x.Match(item));
+
+        if (template is null)
+        {
+            return new Disposable();
+        }
+
+        return template.ItemsSource == null
+            ? new Disposable()
+            : target.Bind(targetProperty, template.ItemsSource);
     }
 }
