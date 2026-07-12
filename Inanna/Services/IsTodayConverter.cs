f4e90e674171362d1508a3460567503fc386525b
gubard <maksimovsergey08@gmail.com>
2026-07-12 21:46:51 +0300

    Add `IsTodayWithoutYearConverter` and `CornerRadius` style for templated controls

--- Inanna/Services/IsTodayConverter.cs
+++ Inanna/Services/IsTodayConverter.cs
@@ -4,6 +4,42 @@ using Gaia.Helpers;
 
 namespace Inanna.Services;
 
+public sealed class IsTodayWithoutYearConverter : IValueConverter
+{
+    public static readonly IsTodayWithoutYearConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var now = DateTime.Now.ToDateOnly();
+
+        if (value is DateOnly date)
+        {
+            var day = Math.Min(DateTime.DaysInMonth(now.Year, date.Month), date.Day);
+
+            return new DateOnly(now.Year, date.Month, day) == now;
+        }
+
+        if (value is DateTime dateTime)
+        {
+            var day = Math.Min(DateTime.DaysInMonth(now.Year, dateTime.Month), dateTime.Day);
+
+            return new DateOnly(now.Year, dateTime.Month, day) == now;
+        }
+
+        return value;
+    }
+
+    public object? ConvertBack(
+        object? value,
+        Type targetType,
+        object? parameter,
+        CultureInfo culture
+    )
+    {
+        throw new NotSupportedException();
+    }
+}
+
 public sealed class IsTodayConverter : IValueConverter
 {
     public static readonly IsTodayConverter Instance = new();
