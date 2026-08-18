24e976b791186684c78827f74129d613c39d89ff
gubard <maksimovsergey08@gmail.com>
2026-08-18 19:26:06 +0300

    Add `ToGrayscale` method to `BitmapExtension` for grayscale bitmap conversion

--- Inanna/Helpers/BitmapExtension.cs
+++ Inanna/Helpers/BitmapExtension.cs
@@ -1,11 +1,75 @@
 using System.IO.Compression;
 using Avalonia;
 using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 
 namespace Inanna.Helpers;
 
 public static class BitmapExtension
 {
+    public static WriteableBitmap ToGrayscale(this Bitmap source)
+    {
+        var pixelSize = source.PixelSize;
+        var dpi = source.Dpi;
+
+        var writable = new WriteableBitmap(
+            pixelSize,
+            dpi,
+            PixelFormat.Bgra8888,
+            AlphaFormat.Premul
+        );
+
+        var width = pixelSize.Width;
+        var height = pixelSize.Height;
+        var stride = width * 4;
+        var pixelData = new byte[height * stride];
+
+        unsafe
+        {
+            fixed (byte* pPixels = pixelData)
+            {
+                source.CopyPixels(
+                    new PixelRect(0, 0, width, height),
+                    (nint)pPixels,
+                    pixelData.Length,
+                    stride
+                );
+            }
+
+            for (var i = 0; i < pixelData.Length; i += 4)
+            {
+                var b = pixelData[i];
+                var g = pixelData[i + 1];
+                var r = pixelData[i + 2];
+
+                var gray = (byte)(0.299 * r + 0.587 * g + 0.114 * b);
+
+                pixelData[i] = gray;
+                pixelData[i + 1] = gray;
+                pixelData[i + 2] = gray;
+            }
+
+            using var fb = writable.Lock();
+            var dstPtr = (byte*)fb.Address;
+            var rowBytes = fb.RowBytes;
+
+            fixed (byte* srcPtr = pixelData)
+            {
+                for (var y = 0; y < height; y++)
+                {
+                    Buffer.MemoryCopy(
+                        srcPtr + (y * stride),
+                        dstPtr + (y * rowBytes),
+                        rowBytes,
+                        stride
+                    );
+                }
+            }
+        }
+
+        return writable;
+    }
+
     public static Stream ToStreamNoCompression(this Bitmap bitmap)
     {
         var stream = new MemoryStream();
