ee6e905c04c5bcd974d541a215cc462aea606a56
gubard <maksimovsergey08@gmail.com>
2026-08-28 09:22:45 +0300

    Add `InannaCalendarDatePicker` control and style integration; update `FluentIcons.Avalonia.Fluent` to 2.1.337

---
+++ Inanna/Controls/InannaCalendarDatePicker.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+
+namespace Inanna.Controls;
+
+public sealed class InannaCalendarDatePicker : CalendarDatePicker
+{
+    protected override void OnPointerWheelChanged(PointerWheelEventArgs e) { }
+}
